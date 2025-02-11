137b78b039c7be934e599a3478dca6a8bf609736
nyunja <poptiff@gmail.com>
2025-02-11 17:08:38 +0300

    org: move get forum stats query to query package

--- backend/handlers/stats.go
+++ backend/handlers/stats.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"forum/database"
+	"forum/database/query"
 )
 
 func GetForumStatsHandler(w http.ResponseWriter, r *http.Request) {
-	stats, err := database.GetForumStats()
+	stats, err := query.GetForumStats()
 	if err != nil {
 		errorMessage(w, "Failed to fetch forum stats", http.StatusInternalServerError)
 		return
