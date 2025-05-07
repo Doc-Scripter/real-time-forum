0d9d698b67627835799e56bdbedc762b60b29987
cliffootieno <clifford242526@gmail.com>
2025-05-07 15:10:19 +0300

    refactor: reorganize HTTP route handling and apply CORS middleware
    this prevents recursive calls to auth function which crashes due to this

--- backend/main.go
+++ backend/main.go
@@ -16,35 +16,37 @@ func main() {
 	}
 
 	// API routes - Public
-	http.HandleFunc("/static/", handlers.Static)
-	http.HandleFunc("/", handlers.Index)
-	http.HandleFunc("/api/login", handlers.LoginHandler)
-	http.HandleFunc("/api/register", handlers.RegisterHandler)
-	http.HandleFunc("/api/posts", handlers.GetPostsHandler)
-	http.HandleFunc("/api/categories", handlers.GetCategoriesHandler)
-	http.HandleFunc("/api/posts/", handlers.GetSinglePostHandler)
-	http.HandleFunc("/api/status", handlers.GetForumStatusHandler)
-	http.HandleFunc("/api/messages", handlers.MessageHandler)
-	http.HandleFunc("/api/messaging", handlers.MessageWebSocketHandler)
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/static/", handlers.Static)
+	mux.HandleFunc("/", handlers.Index)
+	mux.HandleFunc("/api/login", handlers.LoginHandler)
+	mux.HandleFunc("/api/register", handlers.RegisterHandler)
+	mux.HandleFunc("/api/posts", handlers.GetPostsHandler)
+	mux.HandleFunc("/api/categories", handlers.GetCategoriesHandler)
+	mux.HandleFunc("/api/posts/", handlers.GetSinglePostHandler)
+	mux.HandleFunc("/api/status", handlers.GetForumStatusHandler)
+	mux.HandleFunc("/api/messages", handlers.MessageHandler)
+	mux.HandleFunc("/api/messaging", handlers.MessageWebSocketHandler)
+	mux.HandleFunc("/api/CurrentUser", handlers.CurrentUserHandler)
 
 
 	// API routes - Protected
 	protectedMux := http.NewServeMux()
+	protectedMux.HandleFunc("/api/auth/status", handlers.AuthStatusHandler)
 	protectedMux.HandleFunc("/api/logout", handlers.LogoutHandler)
 	protectedMux.HandleFunc("/api/posts", handlers.GetPostsHandler)
 	protectedMux.HandleFunc("/api/posts/create", handlers.CreatePostHandler)
 	protectedMux.HandleFunc("/api/comments", handlers.CreateCommentHandler)
 	protectedMux.HandleFunc("/api/likes", handlers.CreatePostLikeDislikeHandler)
-	protectedMux.HandleFunc("/api/auth/status", handlers.AuthStatusHandler)
 	protectedMux.HandleFunc("/api/comments/like", handlers.CreateCommentLikeHandler)
 
 	// Registering routes
-	http.Handle("/api/protected/", middleware.AuthMiddleware(http.StripPrefix("/api/protected", protectedMux)))
+	mux.Handle("/api/protected/", middleware.AuthMiddleware(http.StripPrefix("/api/protected", protectedMux)))
 
 	// Applying CORS middleware to all API routes
-	http.Handle("/api/", middleware.CorsMiddleware(http.DefaultServeMux))
+	handler:= middleware.CorsMiddleware(mux)
 
 	// Start server
 	log.Println("Server started on http://localhost:9111")
-	log.Fatal(http.ListenAndServe(":9111", nil))
+	log.Fatal(http.ListenAndServe(":9111", handler))
 }
